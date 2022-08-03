3ef459f4ef3566f26b044b3abd28d2fb4a66a939
Matheus Felipe <matheusfeliperibas@gmail.com>
2022-08-03 12:41:56 -0300

    created component Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,12 +7,11 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img 
-            className={styles.avatar}
-            src="https://github.com/matheusribas.png"
+          <Avatar 
+            src="https://github.com/diego3g.png"
           />
           <div className={styles.authorInfo}>
-            <strong>Matheus Felipe</strong>
+            <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
           </div>
         </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,7 +10,10 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=58" 
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/matheusribas.png" />
+        <Avatar 
+          src="https://github.com/matheusribas.png"
+        />
+        <img className={styles.avatar} />
         <strong>Matheus Felipe</strong>
         <span>Web Developer</span>
       </div>
