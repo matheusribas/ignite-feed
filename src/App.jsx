a1c48996ad376162d4450247fac43da1532e1ad6
Matheus Felipe <matheusfeliperibas@gmail.com>
2022-08-03 13:33:42 -0300

    created json to posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import { Post } from "./components/Post";
 import styles from "./App.module.css";
 import './global.css';
 
+const posts = [
+  {
+    id: '1',
+    author: {
+      avatarUrl: 'http://github.com/matheusribas.png',
+      name: 'Matheus Felipe',
+      role: 'Web Developer @BoraEncantar'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: '2',
+    author: {
+      avatarUrl: 'http://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educador @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-10 20:05:00')
+  },
+  {
+    id: '3',
+    author: {
+      avatarUrl: 'http://github.com/diego3g.png',
+      name: 'Diego Fernander',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-10 18:30:00')
+  }
+];
+
 export function App() {
   return (
     <div>
@@ -12,8 +57,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => <Post key={post.id} {...post} />)}
         </main>
       </div>
     </div>
